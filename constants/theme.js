a2165f3fc1a13c75d42e8360584cef1e8d8dd6a8
Josue Eguia <eguiajosue9@gmail.com>
2023-05-27 12:16:55 -0500

    Creacion de constantes

---
+++ constants/theme.js
@@ -0,0 +1,12 @@
+const COLORS = {
+  primary: "#e63946",
+  secondary: "#e1e1e1",
+  light: "#f9f9f9",
+  dark: "#141414",
+};
+
+const FONTS = {};
+
+const SIZES = {};
+
+export { COLORS, FONTS, SIZES };
